2a84b1fc56a3dccd344087ba5c12b94ab7ad7250
DouglasAmarelo <d.lopes.nascimento@gmail.com>
2019-09-04 00:45:43 -0300

    :bust_in_silhouette: Profile component

---
+++ src/components/Profile/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Profile = () => (
+	<div className="profile-wrapper">
+		<h1>Douglas Lopes</h1>
+		<h2>Front-end developer</h2>
+		<p>Front-end developer | React | Vtex e Líder técnico da equipe de tecnologia da @jussilabs</p>
+	</div>
+);
+
+export default Profile;
