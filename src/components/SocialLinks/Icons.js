e4946953c672d21be7b91e9137168c175d993bc2
DouglasAmarelo <d.lopes.nascimento@gmail.com>
2019-11-16 00:01:57 -0300

    Redes sociais

---
+++ src/components/SocialLinks/Icons.js
@@ -0,0 +1,18 @@
+// import { LinkedinSquare, Github, Codepen, Twitter, MediumSquare } from 'styled-icons/boxicons-logos/'
+import { Linkedin } from 'styled-icons/fa-brands/Linkedin';
+import { Github } from 'styled-icons/fa-brands/Github';
+import { Codepen } from 'styled-icons/fa-brands/Codepen';
+import { Twitter } from 'styled-icons/fa-brands/Twitter';
+import { Medium } from 'styled-icons/fa-brands/Medium';
+import { Dev } from 'styled-icons/fa-brands/Dev';
+
+const Icons = {
+	Linkedin,
+	Github,
+	Codepen,
+	Twitter,
+	Medium,
+	Dev
+};
+
+export default Icons;
---
+++ src/components/SocialLinks/content.js
@@ -0,0 +1,28 @@
+const links = [
+	{
+		label: 'Linkedin',
+		url: '#Linkedin'
+	},
+	{
+		label: 'Github',
+		url: '#Github'
+	},
+	{
+		label: 'Codepen',
+		url: '#Codepen'
+	},
+	{
+		label: 'Twitter',
+		url: '#Twitter'
+	},
+	{
+		label: 'Medium',
+		url: '#Medium'
+	},
+	{
+		label: 'Dev',
+		url: '#Dev'
+	},
+];
+
+export default links;
