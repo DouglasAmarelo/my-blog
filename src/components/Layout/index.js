b6d5de0f079371fc18ad2ffd2b4369c87eb1f946
DouglasAmarelo <d.lopes.nascimento@gmail.com>
2019-11-15 23:33:00 -0300

    Sidebar

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Profile from '../Profile';
+
+import Sidebar from '../Sidebar';
 
 import GlobalStyles from '../styles/global';
 import * as S from './styled';
@@ -11,9 +12,7 @@ const Layout = ({ children }) => {
 		<S.LayoutWrapper>
 			<GlobalStyles />
 
-			<aside>
-				<Profile />
-			</aside>
+			<Sidebar />
 
 			<S.LayoutMain>{children}</S.LayoutMain>
 		</S.LayoutWrapper>
