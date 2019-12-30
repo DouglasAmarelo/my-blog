dac9791640b888ac7383cace84c581da35e5a039
DouglasAmarelo <d.lopes.nascimento@gmail.com>
2019-12-29 23:32:58 -0300

    Post

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,20 @@ import React from 'react';
 
 import Layout from '../components/Layout';
 import SEO from '../components/seo';
+import PostItem from '../components/PostItem';
 
 const IndexPage = () => (
 	<Layout>
 		<SEO title="Home" />
-		<h1>Home</h1>
+		<PostItem
+			slug="/about"
+			background="#d0d"
+			category="Misc"
+			date="29 de Dezembro de 2019"
+			timeToRead="5"
+			title="Lorem ipsum dolor sit amet consectetur adipisicing elit"
+			description="Vero nobis labore odio ut suscipit et placeat aliquid impedit exercitationem nesciunt, pariatur voluptates beatae aut assumenda"
+		/>
 	</Layout>
 );
 
